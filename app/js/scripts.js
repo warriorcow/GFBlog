cb875ab7323f7b9c39088d27685cc8563b0e97cc
FJ <battersman27@gmail.com>
2019-04-15 17:49:52 +0300

    pre-alpha

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -2,3 +2,14 @@ $('.hamburger-btn').click(function(){
     $(this).toggleClass('active');
     $(this).next().slideToggle(100);
 })
+
+$(document).scroll(function(){
+    var posScroll = $(window).scrollTop();
+    var heightPanel = $('header').innerHeight();
+
+    if (posScroll >= heightPanel) {
+        $('.fixed').addClass('scroll');
+    } else {
+        $('.fixed').removeClass('scroll');
+    }
+});
